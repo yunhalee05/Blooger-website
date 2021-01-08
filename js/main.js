269c4198fda6c3df04f9d287f776b5d59c0c37a8
yunhalee05 <yunhalee0506@naver.com>
2021-01-08 19:04:59 +0900

    make posts

--- js/main.js
+++ js/main.js
@@ -9,4 +9,14 @@ $(document).ready(function(){
 
     })
 
+    //owl-carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay: false,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    });
+
 });
