3267e9167f07f6e29e9e50c8f109ffe42e2e5678
yunhalee05 <yunhalee0506@naver.com>
2021-01-09 16:53:53 +0900

    make sidebar, footer, right, responsive, animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const responsive ={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    414:{
+        items:2
+    },
+    1024:{
+        items:3
+    }
+
+}
+
 $(document).ready(function(){
 
     $nav=$('.nav');
@@ -16,7 +32,19 @@ $(document).ready(function(){
         autoplayTimeout:3000,
         dots:false,
         nav:true,
-        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive:responsive
+    
     });
 
+
+    //click to scroll top
+    $('.move-up span').click(function(){
+        $('html, body').animate({
+            scrollTop:0
+        },1000);
+    })
+
+    //AOS instance
+    AOS.init();
 });
